8da330376a06d7e82ce831b37a340516fe2cfe07
Vaneet Gupta <vaneet.gupta47@gmail.com>
2023-10-01 23:37:24 +0530

    reconciliation

--- src/index.js
+++ src/index.js
@@ -30,8 +30,14 @@ function createTextElement(text) {
 
 /** @jsx LiteReact.createElement  */
 const element = (
-  <h1 style='background: orange; color: white' title='introduction'>
-    <p> LiteReact Library</p>
+  <h1 style='background: red; color: white' title='introduction'>
+    <p
+      onclick={function () {
+        alert('clicked');
+      }}
+    >
+      LiteReact Library
+    </p>
   </h1>
 );
 
@@ -63,7 +69,7 @@ function workLoop(deadline) {
     shouldYield = deadline.timeRemaining() < 1;
   }
 
-  if (!nextUnitOfWork && workInProgressRoot) {
+  if (!nextUnitOfWork && workInProgressFiber) {
     commitRoot();
   }
 
@@ -76,26 +82,8 @@ function performUnitOfWork(fiber) {
   }
 
   const elements = fiber.props.children;
-  let index = 0;
-  let prevSibling = null;
 
-  while (index < elements.length) {
-    const element = elements[index];
-    const newFiber = {
-      type: element.type,
-      props: element.props,
-      parent: fiber,
-      dom: null,
-    };
-
-    if (index == 0) {
-      fiber.child = newFiber;
-    } else {
-      prevSibling.sibling = newFiber;
-    }
-    prevSibling = newFiber;
-    index++;
-  }
+  reconcileChildren(fiber, elements);
 
   if (fiber.child) return fiber.child;
 
@@ -109,31 +97,142 @@ function performUnitOfWork(fiber) {
   }
 }
 
+function reconcileChildren(workInProgressFiber, elements) {
+  let index = 0;
+  let prevSibling = null;
+  let oldFiber =
+    workInProgressFiber.prevCommit && workInProgressFiber.prevCommit.child;
+
+  while (index < elements.length || oldFiber != null) {
+    const element = elements[index];
+    let newFiber = null;
+    const sameType =
+      workInProgressFiber.prevCommit &&
+      element &&
+      element.type == oldFiber.type;
+
+    if (sameType) {
+      // this is update
+      newFiber = {
+        type: oldFiber.type,
+        props: element.props,
+        dom: oldFiber.dom, /////
+        parent: workInProgressFiber,
+        prevCommit: oldFiber,
+        effectTag: 'UPDATE',
+      };
+    }
+    if (element && !sameType) {
+      // addition
+      newFiber = {
+        type: element.type,
+        props: element.props,
+        dom: null, /////
+        parent: workInProgressFiber,
+        prevCommit: null,
+        effectTag: 'PLACEMENT',
+      };
+    }
+    if (oldFiber && !sameType) {
+      // delete
+      oldFiber.effectTag = 'DELETION';
+      deletions.push(oldFiber);
+    }
+
+    if (index == 0) {
+      workInProgressFiber.child = newFiber;
+    } else {
+      prevSibling.sibling = newFiber;
+    }
+
+    if (oldFiber) {
+      oldFiber = oldFiber.sibling;
+    }
+
+    prevSibling = newFiber;
+    index++;
+  }
+}
+
 let nextUnitOfWork = null;
-let workInProgressRoot = null;
+let workInProgressFiber = null;
+let currentRoot = null;
+let deletions = null;
 
 function render(element, container) {
-  workInProgressRoot = {
+  workInProgressFiber = {
     dom: container,
     props: {
       children: [element],
     },
+    prevCommit: currentRoot,
   };
-  nextUnitOfWork = workInProgressRoot;
+  deletions = [];
+  nextUnitOfWork = workInProgressFiber;
 }
 
 function commitRoot() {
-  commitWork(workInProgressRoot.child);
-  workInProgressRoot = null;
+  deletions.forEach(commitWork);
+  commitWork(workInProgressFiber.child);
+  currentRoot = workInProgressFiber;
+  workInProgressFiber = null;
 }
 
-function commitWork(fiber) {
-  if (!fiber) return;
+function commitWork(fiberObject) {
+  if (!fiberObject) return;
 
-  const domParent = fiber.parent.dom;
-  domParent.appendChild(fiber.dom);
-  commitWork(fiber.child);
-  commitWork(fiber.sibling);
+  const domParent = fiberObject.parent.dom;
+
+  if (fiberObject.effectTag === 'PLACEMENT' && fiberObject.dom != null) {
+    domParent.appendChild(fiberObject.dom);
+  } else if (fiberObject.effectTag === 'DELETION') {
+    domParent.removeChild(fiberObject.dom);
+  } else if (fiberObject.effectTag === 'UPDATE' && fiberObject.dom != null) {
+    updateDOM(fiberObject.dom, fiberObject.prevCommit.props, fiberObject.props);
+  }
+  commitWork(fiberObject.child);
+  commitWork(fiberObject.sibling);
+}
+
+const isEvent = (key) => key.startsWith('on');
+const isProperty = (key) => key !== 'children' && !isEvent(key);
+const isNew = (prev, next) => (key) => prev[key] !== next[key];
+const isGone = (prev, next) => (key) => !(key in next);
+
+function updateDOM(dom, prevProps, nextProps) {
+  // Remove old or changed event listeners
+  Object.keys(prevProps)
+    .filter(isEvent)
+    .filter((key) => !(key in nextProps) || isNew(prevProps, nextProps)(key))
+    .forEach((name) => {
+      const eventType = name.toLowerCase().substring(2);
+      dom.removeEventListener(eventType, prevProps[name]);
+    });
+
+  // Add event listeners
+  Object.keys(nextProps)
+    .filter(isEvent)
+    .filter(isNew(prevProps, nextProps))
+    .forEach((name) => {
+      const eventType = name.toLowerCase().substring(2);
+      dom.addEventListener(eventType, nextProps[name]);
+    });
+
+  // Remove old props from dom
+  Object.keys(prevProps)
+    .filter(isProperty)
+    .filter(isGone(prevProps, nextProps))
+    .forEach((name) => {
+      dom[name] = '';
+    });
+
+  // Set new or changed properties
+  Object.keys(nextProps)
+    .filter(isProperty)
+    .filter(isNew(prevProps, nextProps))
+    .forEach((name) => {
+      dom[name] = nextProps[name];
+    });
 }
 
 LiteReact.render(element, container);
